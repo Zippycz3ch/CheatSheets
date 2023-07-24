11fe88723f04dbef24cb4af1aa613dd5bd15ad41
Zippy <39026332+Zippycz3ch@users.noreply.github.com>
2023-07-24 13:56:30 +0200

    postman updates

--- Postman/postman.js
+++ Postman/postman.js
@@ -15,17 +15,37 @@ const boardName2 = pm.environment.get("boardName");
 pm.environment.set("toDoListId", toDoListId);
 
 // Test of elmnt in jsonData
-
-pm.test("Calendar is disabled", () => {
+    
+pm.test("Element is disabled", () => {
     const jsonData = pm.response.json();
     pm.expect(jsonData.prefs.calendarFeedEnabled).to.eql(false);
 });
+
+// Contains iteration data
+
+pm.test("Should contain the customer id", function () {
+    pm.expect(response.json.customerId).to.eql(pm.iterationData.get("customerId"));
+});
+
+// With parseInt
+pm.test("Should contain the product id", function () {
+    pm.expect(response.json.productId).to.eql(parseInt(pm.variables.get("productId")));
+});    
 
 // Length of array
 
 pm.test("length of array is 1", () => {
     pm.response.json().length == 1;    
 });
+
+// Get request ID
+pm.info.requestId
+
+// Set next request
+postman.setNextRequest("Request Name");
+
+// Stoping workflow
+postman.setNextRequest(null);
 
 // Headers
 
@@ -47,3 +67,12 @@ pm.expect(pm.cookies.has('sessionId')).to.be.true;
 // Cookie has value:
 
 pm.expect(pm.cookies.get('sessionId')).to.eql('ad3se3ss8sg7sg3');
+
+//This
+const person = {
+    firstName: 'Jamie',
+    lastName: 'Scott',
+    age: 29,
+    isMaried: false,
+    sayHello: function (){return 'Hello, my name is ' + this.firstName; }
+};
