66dc58272b65742c5c6c54fd2cf11b1891d1658f
Zippy <39026332+Zippycz3ch@users.noreply.github.com>
2023-09-29 12:56:25 +0200

    Newman CLI updates
    Newman CLI updates

--- Postman/newmancli.ps1
+++ Postman/newmancli.ps1
@@ -1,10 +1,17 @@
 # CLI scripts for testing Roger
 # See scopes if you have problem running scripts https://learning.postman.com/docs/sending-requests/variables/#variable-scopes
 
+
+# Section 0
+# Needs user_admin, user_admin_secret, Abp-TenantId 
+
 # Admin Auth
 # Exports admin_access_token into globals.json for later usage
 # All later scripts use globals to import/export variables that are needed for other scripts
 newman run .\scenarios\admin\AuthenticateAdminCLI.postman_collection.json -e .\data\env\devEnvironment.json --export-globals .\data\exports\globals.json
+
+# Section 1
+# These cases need Admin Auth run first
 
 # User E2E
 newman run .\scenarios\user\e2eUserCLI.postman_collection.json -e .\data\env\devEnvironment.json --export-globals .\data\exports\globals.json -g .\data\exports\globals.json
@@ -12,3 +19,8 @@ newman run .\scenarios\user\e2eUserCLI.postman_collection.json -e .\data\env\dev
 # Device E2E
 newman run .\scenarios\device\deviceE2ECLI.postman_collection.json -e .\data\env\devEnvironment.json --export-globals .\data\exports\globals.json -g .\data\exports\globals.json
 
+# Device LightWeight E2E
+newman run .\scenarios\device\deviceLightWeightE2ECLI.postman_collection.json  -e .\data\env\devEnvironment.json --export-globals .\data\exports\globals.json -g .\data\exports\globals.json
+
+# UserSynch
+newman run .\scenarios\userSynchronization\UserSynchronizationE2E.postman_collection.json -e .\data\env\devEnvironment.json --export-globals .\data\exports\globals.json -g .\data\exports\globals.json -d .\data\testData\userSynchAllGroups.csv
